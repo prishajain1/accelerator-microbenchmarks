dd044ce2d954883eac94e9501c2a27e062596b9f
Prisha Jain <prishajain@google.com>
2025-08-26 06:19:12 +0000

    testing branch

--- run_command.sh
+++ run_command.sh
@@ -1,13 +1,13 @@
-export CLUSTER_NAME=#<cluster_name>
-export REGION=#<region>
-export ZONE=#<zone>
-export PROJECT_ID=#<project_id>
+export CLUSTER_NAME="mlperf-v5p"
+export REGION="europe-west4"
+export ZONE="europe-west4-b"
+export PROJECT_ID="cloud-tpu-multipod-dev"
 export DOCKER_IMAGE="us-docker.pkg.dev/cloud-tpu-images/jax-stable-stack/tpu:jax0.5.2-rev1"
 export RUN_ID=$(date +%Y-%m-%d-%H-%M-%S)
 
 # Change as per need (v5p-128/v5p-256)
 export TPU_TYPE=v5p-128
-export GCS_BASE_PATH=#<gcs_base_path>
+export GCS_BASE_PATH="gs://v5p-microbenchmarks"
 export GCS_PATH="${GCS_BASE_PATH}/report_data_${RUN_ID}"
 
 # gcloud setup
@@ -16,14 +16,14 @@ gcloud config set compute/zone ${ZONE}
 gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${REGION} --project ${PROJECT_ID}
 
 # Benchmark variables
-CONFIG_FILE=#<config_file>
+CONFIG_FILE="configs/xlml_v5p_128_utksharma.yaml"
 
 # TODO: change workload name
 WORKLOAD_NAME="prisha-mb-${TPU_TYPE}"
 
 GCS_JSONL_PATH="${GCS_PATH}/${TPU_TYPE}/metrics_report.jsonl"
 GCS_EXCEL_PATH="${GCS_PATH}/${TPU_TYPE}_benchmark_report.xlsx"
-XPK_COMMAND="git clone https://github.com/utkarshsharma1/accelerator-microbenchmarks.git && \
+XPK_COMMAND="git clone -b testing https://github.com/prishajain1/accelerator-microbenchmarks.git && \
 cd accelerator-microbenchmarks && \
 pip install -r requirements.txt && \
 python src/run_benchmark.py \
