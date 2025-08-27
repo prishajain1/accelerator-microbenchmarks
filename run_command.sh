aede018dfc629e29faffe39317724aefebcdb7fd
Prisha Jain <prishajain@google.com>
2025-08-27 07:33:36 +0000

    hlo dump feature testing

--- run_command.sh
+++ run_command.sh
@@ -1,39 +1,44 @@
-export CLUSTER_NAME="mlperf-v5p"
-export REGION="europe-west4"
-export ZONE="europe-west4-b"
-export PROJECT_ID="cloud-tpu-multipod-dev"
+export CLUSTER_NAME="bodaborg-v6e-256-lcscld-c"
+export REGION="southamerica-west1"
+export ZONE="southamerica-west1-a"
+export PROJECT_ID="tpu-prod-env-one-vm"
 export DOCKER_IMAGE="us-docker.pkg.dev/cloud-tpu-images/jax-stable-stack/tpu:jax0.5.2-rev1"
+export TPU_TYPE="v6e-256"
+
+# Config file path within the git repository
+export REPO_CONFIG_FILE="configs/xlml_v6e_256.yaml"
+
+export GCS_BUCKET="v5p-microbenchmarks"
 export RUN_ID=$(date +%Y-%m-%d-%H-%M-%S)
+export GCS_BASE_PATH="gs://${GCS_BUCKET}"
+# Base path for this specific run's reports
+export GCS_REPORT_PATH="${GCS_BASE_PATH}/report_data_${RUN_ID}"
+# Updated HLO dump path to be inside the run's report_data folder
+export GCS_HLO_DUMP_PATH="${GCS_REPORT_PATH}/hlo_dumps"
 
-# Change as per need (v5p-128/v5p-256)
-export TPU_TYPE=v5p-128
-export GCS_BASE_PATH="gs://v5p-microbenchmarks"
-export GCS_PATH="${GCS_BASE_PATH}/report_data_${RUN_ID}"
+export WORKLOAD_NAME="prishajain-mb-${TPU_TYPE}"
 
-# gcloud setup
 gcloud config set project ${PROJECT_ID}
 gcloud config set compute/zone ${ZONE}
 gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${REGION} --project ${PROJECT_ID}
 
-# Benchmark variables
-CONFIG_FILE="configs/xlml_v5p_128_utksharma.yaml"
+GCS_JSONL_PATH="${GCS_REPORT_PATH}/metrics_report.jsonl"
 
-# TODO: change workload name
-WORKLOAD_NAME="prisha-mb-${TPU_TYPE}"
-
-GCS_JSONL_PATH="${GCS_PATH}/${TPU_TYPE}/metrics_report.jsonl"
-GCS_EXCEL_PATH="${GCS_PATH}/${TPU_TYPE}_benchmark_report.xlsx"
-XPK_COMMAND="git clone -b testing https://github.com/prishajain1/accelerator-microbenchmarks.git && \
+XPK_COMMAND="set -e && \
+git clone -b v6e https://github.com/prishajain1/accelerator-microbenchmarks.git && \
 cd accelerator-microbenchmarks && \
 pip install -r requirements.txt && \
+export XLA_FLAGS='--xla_dump_to=${GCS_HLO_DUMP_PATH} --xla_dump_hlo_as_text' && \
 python src/run_benchmark.py \
-  --config=${CONFIG_FILE} \
-  --generate_report \
-  --gcs_jsonl_path=\"${GCS_JSONL_PATH}\" \
-  --tpu_type=\"${TPU_TYPE}\" \
-  --gcs_excel_path=\"${GCS_EXCEL_PATH}\""
-xpk workload create --cluster=${CLUSTER_NAME} --device-type=${TPU_TYPE} --command="${XPK_COMMAND}" --num-slices=1 --docker-image=${DOCKER_IMAGE} --workload=${WORKLOAD_NAME}
+  --config ${REPO_CONFIG_FILE} \
+  --gcs_jsonl_path='${GCS_JSONL_PATH}' \
+  --tpu_type='${TPU_TYPE}'"
 
-# Delete the workload after it finishes
-# xpk workload delete --cluster=${CLUSTER_NAME} --workload=${WORKLOAD_NAME}
+xpk workload create --cluster=${CLUSTER_NAME} --zone=${ZONE} --project=${PROJECT_ID} \
+  --device-type=${TPU_TYPE} \
+  --command="${XPK_COMMAND}" \
+  --num-slices=1 \
+  --docker-image=${DOCKER_IMAGE} \
+  --workload=${WORKLOAD_NAME}
 
+# xpk workload delete --cluster=${CLUSTER_NAME} --zone=${ZONE} --project=${PROJECT_ID} --workload=${WORKLOAD_NAME}
